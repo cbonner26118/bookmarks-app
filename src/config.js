da56f6e78c72b14a7bd9cfeb22dd30f5830285be
Colton Bonner <cbonner26118@gmail.com>
2020-03-10 18:05:31 -0700

    Off site testing.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$.avUeIRgJU6/nVcfcbRV0OVpFCZLnMk0cfgOqXM3AePar92/F2x6y',
 }

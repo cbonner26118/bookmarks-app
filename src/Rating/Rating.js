30c497a5f28380237941455221f85e7407fdba61
Colton Bonner <cbonner26118@gmail.com>
2020-05-15 00:54:54 -0700

    Refactored for use with Bookmarks-Server API backend.

--- src/Rating/Rating.js
+++ src/Rating/Rating.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./Rating.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Rating.css';
 
 export default function Rating(props) {
   const stars = [0, 0, 0, 0, 0].map((_, i) =>
@@ -14,9 +14,5 @@ export default function Rating(props) {
 }
 
 Rating.propTypes = {
-  value: PropTypes.number.isRequired
-};
-
-Rating.defaultProps = {
-  value: 1
+  value: PropTypes.oneOf([1, 2, 3, 4, 5]).isRequired,
 };
